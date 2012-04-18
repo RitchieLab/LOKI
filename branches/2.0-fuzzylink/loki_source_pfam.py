1a164f00efccd96d1b145d73b012f5cb46efaf97
atfrase <atf3@psu.edu>
2012-04-18 20:10:56 +0000

    new 2.0 sub-branch for the fuzzy linking overhaul, so that the current 2.0 branch remains usable for testing and experimentation in the interim
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/LOKI@1926 9e62ecf6-770d-0410-a9d7-e62a33e3d092

---
+++ branches/2.0-fuzzylink/loki_source_pfam.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+import loki_source
+
+
+class Source_pfam(loki_source.Source):
+	
+	
+	# ##################################################
+	# source interface
+	
+	
+	def getDependencies(cls):
+		return ('entrez',)
+	#getDependencies()
+	
+	
+	def download(self):
+		# download the latest source files
+		self.downloadFilesFromFTP('ftp.sanger.ac.uk', {
+			'pfamA.txt.gz':    '/pub/databases/Pfam/current_release/database_files/pfamA.txt.gz',
+			'seq_info.txt.gz': '/pub/databases/Pfam/current_release/database_files/seq_info.txt.gz',
+		})
+	#download()
+	
+	
+	def update(self):
+		# begin transaction to update database
+		self.log("initializing update process ...")
+		with self.bulkUpdateContext(group=True, group_name=True, group_group=True, group_region=True):
+			self.log(" OK\n")
+			
+			# clear out all old data from this source
+			self.log("deleting old records from the database ...")
+			self.deleteSourceData()
+			self.log(" OK\n")
+			
+			# get or create the required metadata records
+			namespaceID = {
+				'uniprot':        self.addNamespace('uniprot'),
+				'pfam':           self.addNamespace('pfam'),
+				'protein_family': self.addNamespace('protein_family'),
+			}
+			relationshipID = {
+				'':               self.addRelationship(''),
+			}
+			typeID = {
+				'gene':           self.addType('gene'),
+				'protein_family': self.addType('protein_family'),
+			}
+			
+			# process protein families
+			self.log("processing protein families ...")
+			pfamFile = self.zfile('pfamA.txt.gz') #TODO:context manager,iterator
+			pfamName = {}
+			pfamDesc = {}
+			groupPFam = {}
+			for line in pfamFile:
+				words = line.split("\t")
+				pfamID = words[1]
+				name = words[2]
+				desc = words[4]
+				group = words[8]
+				
+				pfamName[pfamID] = name
+				pfamDesc[pfamID] = desc
+				if group not in groupPFam:
+					groupPFam[group] = set()
+				groupPFam[group].add(pfamID)
+			numPfam = len(pfamName)
+			numGroup = len(groupPFam)
+			self.log(" OK: %d families, %d categories\n" % (numPfam,numGroup))
+			
+			# store protein families
+			self.log("writing protein families to the database ...")
+			listPFam = pfamName.keys()
+			listGroup = groupPFam.keys()
+			listGID = self.addTypedGroups(typeID['protein_family'], ((pfamName[pfamID],pfamDesc[pfamID]) for pfamID in listPFam))
+			pfamGID = dict(zip(listPFam,listGID))
+			listGID = self.addTypedGroups(typeID['protein_family'], ((group,"") for group in listGroup))
+			groupGID = dict(zip(listGroup,listGID))
+			self.log(" OK\n")
+			
+			# store protein family names
+			self.log("writing protein family names to the database ...")
+			self.addNamespacedGroupNames(namespaceID['pfam'], ((pfamGID[pfamID],pfamID) for pfamID in listPFam))
+			self.addNamespacedGroupNames(namespaceID['protein_family'], ((pfamGID[pfamID],pfamName[pfamID]) for pfamID in listPFam))
+			self.addNamespacedGroupNames(namespaceID['pfam'], ((groupGID[group],group) for group in listGroup))
+			self.log(" OK\n")
+			
+			# store protein family meta-group links
+			self.log("writing protein family links to the database ...")
+			for group in groupPFam:
+				self.addGroupGroups( (pfamGID[pfamID],groupGID[group],relationshipID['']) for pfamID in groupPFam[group] )
+			self.log(" OK\n")
+			
+			# process associations
+			self.log("processing gene associations ...")
+			assocFile = self.zfile('seq_info.txt.gz') #TODO:context manager,iterator
+			setAssoc = set()
+			setFlagAssoc = set()
+			setOrphan = set()
+			setAmbig = set()
+			setUnrec = set()
+			for line in assocFile:
+				words = line.split("\t")
+				if len(words) < 6:
+					continue
+				pfamID = words[0]
+				uniprotAcc = words[5]
+				uniprotID = words[6]
+				species = words[8]
+				
+				if species == 'Homo sapiens (Human)':
+					if pfamID not in pfamGID:
+						setOrphan.add( (pfamID,uniprotAcc,uniprotID) )
+					else:
+						regionIDs = self._loki.getRegionIDsByNames(
+								[uniprotAcc, uniprotID],
+								[namespaceID['uniprot'], namespaceID['uniprot']],
+								typeID['gene'],
+								self._loki.MATCH_BEST
+						)
+						if len(regionIDs) == 1:
+							setAssoc.add( (pfamGID[pfamID],regionIDs[0]) )
+						elif len(regionIDs) > 1:
+							setAmbig.add( (pfamGID[pfamID],uniprotAcc,uniprotID) )
+							for regionID in regionIDs:
+								setFlagAssoc.add( (pfamGID[pfamID],regionID) )
+						else:
+							setUnrec.add( (pfamGID[pfamID],uniprotAcc,uniprotID) )
+				#if association is ok
+			#foreach association
+			numAssoc = len(setAssoc)
+			numGene = len(set(assoc[1] for assoc in setAssoc))
+			numGroup = len(set(assoc[0] for assoc in setAssoc))
+			self.log(" OK: %d associations (%d genes in %d groups)\n" % (numAssoc,numGene,numGroup))
+			self.logPush()
+			if setOrphan:
+				numAssoc = len(setOrphan)
+				numName = len(set(assoc[1:] for assoc in setOrphan))
+				numGroup = len(set(assoc[0] for assoc in setOrphan))
+				self.log("WARNING: %d orphaned associations (%d identifiers in %d groups)\n" % (numAssoc,numName,numGroup))
+			if setAmbig:
+				numAssoc = len(setAmbig)
+				numName = len(set(assoc[1:] for assoc in setAmbig))
+				numGroup = len(set(assoc[0] for assoc in setAmbig))
+				self.log("WARNING: %d ambiguous associations (%d identifiers in %d groups)\n" % (numAssoc,numName,numGroup))
+			if setUnrec:
+				numAssoc = len(setUnrec)
+				numName = len(set(assoc[1:] for assoc in setUnrec))
+				numGroup = len(set(assoc[0] for assoc in setUnrec))
+				self.log("WARNING: %d unrecognized associations (%d identifiers in %d groups)\n" % (numAssoc,numName,numGroup))
+			self.logPop()
+			
+			# store gene associations
+			self.log("writing gene associations to the database ...")
+			self.addGroupRegions(setAssoc)
+			# TODO: setFlagAssoc
+			self.log(" OK\n")
+			
+			# commit transaction
+			self.log("finalizing update process ...")
+		#with bulk update
+		self.log(" OK\n")
+	#update()
+	
+#Source_pfam
---
+++ branches/2.0-fuzzylink/loki_source_pharmgkb.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+
+import zipfile
+import loki_source
+
+
+class Source_pharmgkb(loki_source.Source):
+	
+	
+	# ##################################################
+	# source interface
+	
+	
+	def getDependencies(cls):
+		return ('entrez',)
+	#getDependencies()
+	
+	
+	def download(self):
+		# download the latest source files
+		return
+		self.downloadFilesFromHTTP('www.pharmgkb.org', {
+			'genes.zip':        '/commonFileDownload.action?filename=genes.zip',
+			'pathways-tsv.zip': '/commonFileDownload.action?filename=pathways-tsv.zip',
+		})
+	#download()
+	
+	
+	def update(self):
+		# begin transaction to update database
+		self.log("initializing update process ...")
+		with self.bulkUpdateContext(group=True, group_name=True, group_region=True):
+			self.log(" OK\n")
+			
+			# clear out all old data from this source
+			self.log("deleting old records from the database ...")
+			self.deleteSourceData()
+			self.log(" OK\n")
+			
+			# get or create the required metadata records
+			namespaceID = {
+				'gene':     self.addNamespace('gene'),
+				'entrez':   self.addNamespace('entrez'),
+				'refseq':   self.addNamespace('refseq'),
+				'ensembl':  self.addNamespace('ensembl'),
+				'hgnc':     self.addNamespace('hgnc'),
+				'uniprot':  self.addNamespace('uniprot'),
+				'pharmgkb': self.addNamespace('pharmgkb'),
+			}
+			typeID = {
+				'gene':     self.addType('gene'),
+				'pathway':  self.addType('pathway'),
+			}
+			
+			# process gene names
+			self.log("verifying gene name archive file ...")
+			setAssoc = set()
+			setAmbig = set()
+			setUnrec = set()
+			empty = tuple()
+			with zipfile.ZipFile('genes.zip','r') as geneZip:
+				err = geneZip.testzip()
+				if err:
+					self.log(" ERROR\n")
+					self.log("CRC failed for %s\n" % err)
+					return False
+				self.log(" OK\n")
+				self.log("processing gene names ...")
+				xrefNS = {
+					'entrezGene':    'entrez',
+					'refSeqDna':     'refseq',
+					'refSeqRna':     'refseq',
+					'refSeqProtein': 'refseq',
+					'ensembl':       'ensembl',
+					'hgnc':          'hgnc',
+					'uniProtKb':     'uniprot',
+				}
+				for info in geneZip.infolist():
+					if info.filename == 'genes.tsv':
+						geneFile = geneZip.open(info,'r')
+						header = geneFile.next().rstrip()
+						if header != "PharmGKB Accession Id	Entrez Id	Ensembl Id	Name	Symbol	Alternate Names	Alternate Symbols	Is VIP	Has Variant Annotation	Cross-references":
+							self.log(" ERROR\n")
+							self.log("unrecognized file header in '%s': %s\n" % (info.filename,header))
+							return False
+						for line in geneFile:
+							words = line.split("\t")
+							pgkbID = words[0]
+							entrezID = words[1]
+							ensemblID = words[2]
+							name = words[4]
+							aliases = words[6].split(',') if words[6] != "" else empty
+							xrefs = words[9].strip(', ').split(',') if words[9] != "" else empty
+							
+							nameList = []
+							nsList = []
+							if entrezID:
+								nameList.append(entrezID)
+								nsList.append(namespaceID['entrez'])
+							if ensemblID:
+								nameList.append(ensemblID)
+								nsList.append(namespaceID['ensembl'])
+							if name:
+								nameList.append(name)
+								nsList.append(namespaceID['gene'])
+							for alias in aliases:
+								nameList.append(alias.strip('" '))
+								nsList.append(namespaceID['gene'])
+							for xref in xrefs:
+								try:
+									xrefDB,xrefID = xref.split(':',1)
+									if xrefDB in xrefNS:
+										nameList.append(xrefID)
+										nsList.append(namespaceID[xrefNS[xrefDB]])
+								except ValueError:
+									pass
+							regionIDs = self._loki.getRegionIDsByNames(nameList, nsList, typeID['gene'], self._loki.MATCH_BEST)
+							if len(regionIDs) == 1:
+								setAssoc.add( (regionIDs[0],pgkbID) )
+							elif len(regionIDs) > 1:
+								setAmbig.add( (pgkbID,) + tuple(nameList) )
+							else:
+								setUnrec.add( (pgkbID,) + tuple(nameList) )
+						#foreach line in geneFile
+						geneFile.close()
+					#if genes.tsv
+				#foreach file in geneZip
+			#with geneZip
+			numAssoc = len(setAssoc)
+			numGene = len(set(assoc[0] for assoc in setAssoc))
+			numName = len(set(assoc[1] for assoc in setAssoc))
+			self.log(" OK: %d associations (%d identifiers for %d genes)\n" % (numAssoc,numGene,numName))
+			self.logPush()
+			if setAmbig:
+				numAssoc = len(setAmbig)
+				numName = len(set(assoc[1:] for assoc in setAmbig))
+				self.log("WARNING: %d ambiguous associations (%d identifiers)\n" % (numAssoc,numName))
+			if setUnrec:
+				numAssoc = len(setUnrec)
+				numName = len(set(assoc[1:] for assoc in setUnrec))
+				self.log("WARNING: %d unrecognized associations (%d identifiers)\n" % (numAssoc,numName))
+			self.logPop()
+			
+			# store gene names
+			self.log("writing gene names to the database ...")
+			self.addNamespacedRegionNames(namespaceID['pharmgkb'], setAssoc)
+			self.log(" OK\n")
+			
+			# process pathways
+			self.log("verifying pathway archive file ...")
+			pathDesc = {}
+			setAssoc = set()
+			setFlagAssoc = set()
+			setAmbig = set()
+			setUnrec = set()
+			with zipfile.ZipFile('pathways-tsv.zip','r') as pathZip:
+				err = pathZip.testzip()
+				if err:
+					self.log(" ERROR\n")
+					self.log("CRC failed for %s\n" % err)
+					return False
+				self.log(" OK\n")
+				self.log("processing pathways ...")
+				for info in pathZip.infolist():
+					if info.filename == 'pathways.tsv':
+						pathFile = pathZip.open(info,'r')
+						curPath = None
+						for line in pathFile:
+							line = line.rstrip("\r\n")
+							if line == "" and lastline == "":
+								curPath = None
+							elif curPath == None:
+								words = line.split(':')
+								if len(words) >= 2:
+									curPath = words[0].strip()
+									pathDesc[curPath] = words[1].strip()
+							elif curPath == False:
+								pass
+							else:
+								words = line.split("\t")
+								if words[0] == "From":
+									curPath = False
+								elif words[0] == "Gene":
+									pgkbID = words[1]
+									gene = words[2]
+									
+									nameList = [pgkbID, gene]
+									nsList = [namespaceID['pharmgkb'], namespaceID['gene']]
+									regionIDs = self._loki.getRegionIDsByNames(nameList, nsList, typeID['gene'], self._loki.MATCH_BEST)
+									if len(regionIDs) == 1:
+										setAssoc.add( (curPath,regionIDs[0]) )
+									elif len(regionIDs) > 1:
+										setAmbig.add( (curPath,pgkbID,gene) )
+										for regionID in regionIDs:
+											setFlagAssoc.add( (curPath,regionID) )
+									else:
+										setUnrec.add( (curPath,pgkbID,gene) )
+								#if assoc is Gene
+							lastline = line
+						#foreach line in pathFile
+						pathFile.close()
+					#if pathways.tsv
+				#foreach file in pathZip
+			#with pathZip
+			numAssoc = len(setAssoc)
+			numGene = len(set(assoc[1] for assoc in setAssoc))
+			numGroup = len(set(assoc[0] for assoc in setAssoc))
+			self.log(" OK: %d associations (%d genes in %d groups)\n" % (numAssoc,numGene,numGroup))
+			self.logPush()
+			if setAmbig:
+				numAssoc = len(setAmbig)
+				numName = len(set(assoc[1:] for assoc in setAmbig))
+				numGroup = len(set(assoc[0] for assoc in setAmbig))
+				self.log("WARNING: %d ambiguous associations (%d identifiers in %d groups)\n" % (numAssoc,numName,numGroup))
+			if setUnrec:
+				numAssoc = len(setUnrec)
+				numName = len(set(assoc[1:] for assoc in setUnrec))
+				numGroup = len(set(assoc[0] for assoc in setUnrec))
+				self.log("WARNING: %d unrecognized associations (%d identifiers in %d groups)\n" % (numAssoc,numName,numGroup))
+			self.logPop()
+			
+			# store pathways
+			self.log("writing pathways to the database ...")
+			listPath = pathDesc.keys()
+			listGID = self.addTypedGroups(typeID['pathway'], ((path,pathDesc[path]) for path in listPath))
+			pathGID = dict(zip(listPath,listGID))
+			self.log(" OK\n")
+			
+			# store pathway names
+			self.log("writing pathway names to the database ...")
+			self.addNamespacedGroupNames(namespaceID['pharmgkb'], ((pathGID[path],path) for path in listPath))
+			self.log(" OK\n")
+			
+			# store gene associations
+			self.log("writing gene associations to the database ...")
+			self.addGroupRegions( (pathGID[assoc[0]],assoc[1]) for assoc in setAssoc )
+			self.log(" OK\n")
+			
+			#TODO: diseases,drugs,relationships?
+			
+			# commit transaction
+			self.log("finalizing update process ...")
+		#with bulk update
+		self.log(" OK\n")
+	#update()
+	
+#Source_pharmgkb
