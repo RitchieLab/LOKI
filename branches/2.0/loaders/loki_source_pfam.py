ace1428feb497000b965b27362af59027754b6f1
atfrase <atf3@psu.edu>
2012-08-21 13:56:49 +0000

    more descriptive group labels
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/LOKI@2316 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- branches/2.0/loaders/loki_source_pfam.py
+++ branches/2.0/loaders/loki_source_pfam.py
@@ -38,46 +38,50 @@ class Source_pfam(loki_source.Source):
 		# process protein families
 		self.log("processing protein families ...")
 		pfamFile = self.zfile('pfamA.txt.gz') #TODO:context manager,iterator
-		groupPFam = {}
-		pfamName = {}
-		pfamDesc = {}
+		groupAcc = {}
+		accID = {}
+		accName = {}
+		accDesc = {}
 		for line in pfamFile:
-			words = line.split("\t")
-			pfamID = words[1]
-			name = words[2]
-			desc = words[4]
-			group = words[8]
+			words = line.decode('latin-1').split("\t")
+			pfamAcc = words[1].strip()
+			pfamID = words[2].strip()
+			name = words[4].strip()
+			group = words[8].strip()
+			desc = words[9].strip()
 			
-			if group not in groupPFam:
-				groupPFam[group] = set()
-			groupPFam[group].add(pfamID)
-			pfamName[pfamID] = name
-			pfamDesc[pfamID] = desc
-		numGroup = len(groupPFam)
-		numPfam = len(pfamName)
-		self.log(" OK: %d categories, %d families\n" % (numGroup,numPfam))
+			if group not in groupAcc:
+				groupAcc[group] = set()
+			groupAcc[group].add(pfamAcc)
+			accID[pfamAcc] = pfamID
+			accName[pfamAcc] = name
+			accDesc[pfamAcc] = desc
+		numGroup = len(groupAcc)
+		numAcc = len(accName)
+		self.log(" OK: %d categories, %d families\n" % (numGroup,numAcc))
 		
 		# store protein families
 		self.log("writing protein families to the database ...")
-		listGroup = groupPFam.keys()
+		listGroup = groupAcc.keys()
 		listGID = self.addTypedGroups(typeID['proteinfamily'], ((group,"") for group in listGroup))
 		groupGID = dict(zip(listGroup,listGID))
-		listPFam = pfamName.keys()
-		listGID = self.addTypedGroups(typeID['proteinfamily'], ((pfamName[pfamID],pfamDesc[pfamID]) for pfamID in listPFam))
-		pfamGID = dict(zip(listPFam,listGID))
+		listAcc = accID.keys()
+		listGID = self.addTypedGroups(typeID['proteinfamily'], ((accName[pfamAcc],accDesc[pfamAcc]) for pfamAcc in listAcc))
+		accGID = dict(zip(listAcc,listGID))
 		self.log(" OK\n")
 		
 		# store protein family names
 		self.log("writing protein family names to the database ...")
 		self.addGroupNamespacedNames(namespaceID['pfam_id'], ((groupGID[group],group) for group in listGroup))
-		self.addGroupNamespacedNames(namespaceID['pfam_id'], ((pfamGID[pfamID],pfamID) for pfamID in listPFam))
-		self.addGroupNamespacedNames(namespaceID['proteinfamily'], ((pfamGID[pfamID],pfamName[pfamID]) for pfamID in listPFam))
+		self.addGroupNamespacedNames(namespaceID['pfam_id'], ((accGID[pfamAcc],pfamAcc) for pfamAcc in listAcc))
+		self.addGroupNamespacedNames(namespaceID['proteinfamily'], ((accGID[pfamAcc],accID[pfamAcc]) for pfamAcc in listAcc))
+		self.addGroupNamespacedNames(namespaceID['proteinfamily'], ((accGID[pfamAcc],accName[pfamAcc]) for pfamAcc in listAcc))
 		self.log(" OK\n")
 		
 		# store protein family meta-group links
 		self.log("writing protein family links to the database ...")
-		for group in groupPFam:
-			self.addGroupRelationships( (pfamGID[pfamID],groupGID[group],relationshipID['']) for pfamID in groupPFam[group] )
+		for group in groupAcc:
+			self.addGroupRelationships( (accGID[pfamAcc],groupGID[group],relationshipID['']) for pfamAcc in groupAcc[group] )
 		self.log(" OK\n")
 		
 		# process associations
@@ -89,16 +93,16 @@ class Source_pfam(loki_source.Source):
 			words = line.split("\t")
 			if len(words) < 6:
 				continue
-			pfamID = words[0]
+			pfamAcc = words[0]
 			uniprotAcc = words[5]
 			uniprotID = words[6]
 			species = words[8]
 			
-			if pfamID in pfamGID and species == 'Homo sapiens (Human)':
+			if pfamAcc in accGID and species == 'Homo sapiens (Human)':
 				numAssoc += 1
 				numID += 2
-				setAssoc.add( (pfamGID[pfamID],numAssoc,uniprotAcc) )
-				setAssoc.add( (pfamGID[pfamID],numAssoc,uniprotID) )
+				setAssoc.add( (accGID[pfamAcc],numAssoc,uniprotAcc) )
+				setAssoc.add( (accGID[pfamAcc],numAssoc,uniprotID) )
 			#if association is ok
 		#foreach association
 		self.log(" OK: %d associations (%d identifiers)\n" % (numAssoc,numID))
