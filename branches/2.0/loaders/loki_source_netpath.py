b9293dda95f560d5cb97522f534f26ce18b43301
atfrase <atf3@psu.edu>
2012-08-21 13:52:01 +0000

    stopped using the file last updated in 2008, switch to only using the more recent one; decode source file using latin-1 charset
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/LOKI@2311 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- branches/2.0/loaders/loki_source_netpath.py
+++ branches/2.0/loaders/loki_source_netpath.py
@@ -10,8 +10,8 @@ class Source_netpath(loki_source.Source):
 	def download(self, options):
 		# download the latest source files
 		self.downloadFilesFromHTTP('www.netpath.org', {
+		#	'NetPath_GeneReg_TSV.zip':  '/data/batch/NetPath_GeneReg_TSV.zip', #Last-Modified: Fri, 31 Oct 2008 17:00:16 GMT
 			'NetPath_GeneReg_TSV1.zip': '/data/batch/NetPath_GeneReg_TSV1.zip', #Last-Modified: Sat, 03 Sep 2011 10:07:03 GMT
-			'NetPath_GeneReg_TSV.zip':  '/data/batch/NetPath_GeneReg_TSV.zip', #Last-Modified: Fri, 31 Oct 2008 17:00:16 GMT
 		})
 	#download()
 	
@@ -34,11 +34,14 @@ class Source_netpath(loki_source.Source):
 			('gene',),
 		])
 		
-		# process pathways
-		# (this file has associations too, but fewer of them, so we just
-		#  use it for its pathway labels, which the other file lacks)
-		self.log("verifying pathway archive ...")
+		# process pathways and associations
+		self.log("verifying archive ...")
 		pathName = {}
+		nsAssoc = {
+			'symbol'     : set(),
+			'entrez_gid' : set(),
+		}
+		numAssoc = 0
 		with zipfile.ZipFile('NetPath_GeneReg_TSV1.zip','r') as pathZip:
 			err = pathZip.testzip()
 			if err:
@@ -47,25 +50,34 @@ class Source_netpath(loki_source.Source):
 				return False
 			self.log(" OK\n")
 			self.log("processing pathways ...")
-			for info in pathZip.infolist(): # there should be only one
-				pathFile = pathZip.open(info,'rU')
-				header = pathFile.next().rstrip()
-				if header != "Gene regulation id	Pathway name	Pathway ID	Gene name	Entrez gene ID	Regulation	Experiment	PubMed ID":
-					self.log(" ERROR\n")
-					self.log("unrecognized file header in '%s': %s\n" % (info.filename,header))
-					return False
-				for line in pathFile:
-					words = line.split("\t")
-					name = words[1]
-					pathID = words[2]
-					
-					pathName[pathID] = name
-				#foreach line in pathFile
-				pathFile.close()
+			for info in pathZip.infolist():
+				# there should be only one, but just in case..
+				if info.filename == 'NetPath_Gene_regulation_all.txt':
+					pathFile = pathZip.open(info,'rU')
+					header = pathFile.next().rstrip()
+					if header != "Gene regulation id	Pathway name	Pathway ID	Gene name	Entrez gene ID	Regulation	Experiment	PubMed ID":
+						self.log(" ERROR\n")
+						self.log("unrecognized file header in '%s': %s\n" % (info.filename,header))
+						return False
+					for line in pathFile:
+						words = line.decode('latin-1').split("\t")
+						pathway = words[1]
+						pathID = words[2]
+						gene = words[3].strip()
+						entrezID = words[4]
+						
+						pathName[pathID] = pathway
+						numAssoc += 1
+						nsAssoc['entrez_gid'].add( (pathID,numAssoc,entrezID) )
+						nsAssoc['symbol'].add( (pathID,numAssoc,gene) )
+					#foreach line in pathFile
+					pathFile.close()
+				#if file is the one we want
 			#foreach file in pathZip
-			numPathways = len(pathName)
-			self.log(" OK: %d pathways\n" % (numPathways,))
 		#with pathZip
+		numPathways = len(pathName)
+		numID = sum(len(nsAssoc[ns]) for ns in nsAssoc)
+		self.log(" OK: %d pathways, %d associations (%d identifiers)\n" % (numPathways,numAssoc,numID))
 		
 		# store pathways
 		self.log("writing pathways to the database ...")
@@ -80,50 +92,10 @@ class Source_netpath(loki_source.Source):
 		self.addGroupNamespacedNames(namespaceID['pathway'], ((pathGID[pathID],pathName[pathID]) for pathID in listPath))
 		self.log(" OK\n")
 		
-		# process associations
-		self.log("verifying gene association archive ...")
-		nsAssoc = {
-			'symbol':     set(),
-			'entrez_gid': set(),
-		}
-		numAssoc = numID = 0
-		with zipfile.ZipFile('NetPath_GeneReg_TSV.zip','r') as pathZip:
-			err = pathZip.testzip()
-			if err:
-				self.log(" ERROR\n")
-				self.log("CRC failed for %s\n" % err)
-				return False
-			self.log(" OK\n")
-			self.log("processing gene associations ...")
-			for info in pathZip.infolist():
-				pathFile = pathZip.open(info,'r')
-				header = pathFile.next().rstrip()
-				if header != "Gene regulation id	Pathway name	Pathway id	Gene Name	Entrez Gene	Regulation	Experiment type	PubMed id":
-					self.log(" ERROR\n")
-					self.log("unrecognized file header in '%s': %s\n" % (info.filename,header))
-					return False
-				for line in pathFile:
-					words = line.split("\t")
-					pathID = words[2]
-					gene = words[3]
-					entrezID = words[4]
-					
-					if pathID in pathGID:
-						numAssoc += 1
-						numID += 2
-						nsAssoc['entrez_gid'].add( (pathGID[pathID],numAssoc,entrezID) )
-						nsAssoc['symbol'].add( (pathGID[pathID],numAssoc,gene) )
-					#if pathway is ok
-				#foreach line in pathFile
-				pathFile.close()
-			#foreach file in pathZip
-		#with pathZip
-		self.log(" OK: %d associations (%d identifiers)\n" % (numAssoc,numID))
-		
 		# store gene associations
 		self.log("writing gene associations to the database ...")
 		for ns in nsAssoc:
-			self.addGroupMemberTypedNamespacedNames(typeID['gene'], namespaceID[ns], nsAssoc[ns])
+			self.addGroupMemberTypedNamespacedNames(typeID['gene'], namespaceID[ns], ((pathGID[assoc[0]],assoc[1],assoc[2]) for assoc in nsAssoc[ns]))
 		self.log(" OK\n")
 	#update()
 	
