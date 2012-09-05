6909fecafc3fb53536adddab438ae0d26eed43c8
atfrase <atf3@psu.edu>
2012-09-05 13:18:49 +0000

    2.0a2 - renamed option 'snp-loci' to just 'loci' - added options 'merges' and 'roles' ('yes' or 'no') to include or skip those data, default 'yes'
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/LOKI@2360 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- branches/2.0/loaders/loki_source_dbsnp.py
+++ branches/2.0/loaders/loki_source_dbsnp.py
@@ -42,31 +42,40 @@ class Source_dbsnp(loki_source.Source):
 	
 	@classmethod
 	def getVersionString(cls):
-		return '2.0a1 (2012-08-30)'
+		return '2.0a2 (2012-09-04)'
 	#getVersionString()
 	
 	
 	@classmethod
 	def getOptions(cls):
 		return {
-			'snp-loci': '[all|validated]  --  store all or only validated SNP loci (default: all)'
+			'loci'   : '[all|validated]  --  store all or only validated SNP loci (default: all)',
+			'merges' : '[yes|no]  --  process and store RS# merge history (default: yes)',
+			'roles'  : '[yes|no]  --  process and store SNP roles (default: yes)',
 		}
 	#getOptions()
 	
 	
 	def validateOptions(self, options):
 		for o,v in options.iteritems():
-			if o == 'snp-loci':
-				v = v.strip().lower()
+			v = v.strip().lower()
+			if o == 'loci':
 				if 'all'.startswith(v):
 					v = 'all'
 				elif 'validated'.startswith(v):
 					v = 'validated'
 				else:
-					return "snp-loci must be 'all' or 'validated'"
-				options[o] = v
+					return "loci must be 'all' or 'validated'"
+			elif o in ('roles','merges'):
+				if 'yes'.startswith(v):
+					v = 'yes'
+				elif 'no'.startswith(v):
+					v = 'no'
+				else:
+					return "%s must be 'yes' or 'no'" % o
 			else:
 				return "unknown option '%s'" % o
+			options[o] = v
 		return True
 	#validateOptions()
 	
@@ -74,18 +83,20 @@ class Source_dbsnp(loki_source.Source):
 	def download(self, options):
 		# define a callback to identify the latest SNPContigLocusId file
 		def remFilesCallback(ftp):
-			remFiles = {
-				'RsMergeArch.bcp.gz'           : '/snp/organisms/human_9606/database/organism_data/RsMergeArch.bcp.gz',
-				'SnpFunctionCode.bcp.gz'       : '/snp/organisms/human_9606/database/shared_data/SnpFunctionCode.bcp.gz',
-			}
+			remFiles = dict()
 			for chm in self._chmList:
 				remFiles['chr_%s.txt.gz' % chm] = '/snp/organisms/human_9606/chr_rpts/chr_%s.txt.gz' % chm
 			
-			path = '/snp/organisms/human_9606/database/organism_data'
-			ftp.cwd(path)
-			bestfile = self._identifyLatestSNPContig(ftp.nlst())
-			if bestfile:
-				remFiles[bestfile] = '%s/%s' % (path,bestfile)
+			if options.get('merges',True):
+				remFiles['RsMergeArch.bcp.gz'] = '/snp/organisms/human_9606/database/organism_data/RsMergeArch.bcp.gz'
+			
+			if options.get('roles',True):
+				remFiles['SnpFunctionCode.bcp.gz'] = '/snp/organisms/human_9606/database/shared_data/SnpFunctionCode.bcp.gz'
+				path = '/snp/organisms/human_9606/database/organism_data'
+				ftp.cwd(path)
+				bestfile = self._identifyLatestSNPContig(ftp.nlst())
+				if bestfile:
+					remFiles[bestfile] = '%s/%s' % (path,bestfile)
 			
 			return remFiles
 		#remFilesCallback
@@ -102,41 +113,44 @@ class Source_dbsnp(loki_source.Source):
 		self.log(" OK\n")
 		
 		# process merge report (no header!)
-		self.log("processing SNP merge records ...")
-		mergeFile = self.zfile('RsMergeArch.bcp.gz') #TODO:context manager,iterator
-		numMerge = 0
-		listMerge = list()
-		for line in mergeFile:
-			words = line.split("\t")
-			if len(words) <= 6:
-				continue
-			rsOld = long(words[0])
-			#rsNew = long(words[1])
-			rsCur = long(words[6])
+		if options.get('merges','yes') == 'yes':
+			self.log("processing SNP merge records ...")
+			mergeFile = self.zfile('RsMergeArch.bcp.gz') #TODO:context manager,iterator
+			numMerge = 0
+			listMerge = list()
+			for line in mergeFile:
+				words = line.split("\t")
+				if len(words) <= 6:
+					continue
+				rsOld = long(words[0])
+				#rsNew = long(words[1])
+				rsCur = long(words[6])
+				
+				numMerge += 1
+				listMerge.append( (rsOld,rsCur) )
+				
+				# write to the database after each million, to keep memory usage down
+				if len(listMerge) >= 1000000:
+					self.log(" %1.f million so far\n" % (numMerge/1000000.0)) #TODO: time estimate
+					self.log("writing SNP merge records to the database ...")
+					self.addSNPMerges(listMerge)
+					listMerge = list()
+					self.log(" OK\n")
+					self.log("processing SNP merge records ...")
+			#foreach line in mergeFile
+			self.log(" OK: %d merged RS#s\n" % numMerge)
 			
-			numMerge += 1
-			listMerge.append( (rsOld,rsCur) )
-			
-			# write to the database after each million, to keep memory usage down
-			if len(listMerge) >= 1000000:
-				self.log(" %1.f million so far\n" % (numMerge/1000000.0)) #TODO: time estimate
+			# write any remaining records
+			if listMerge:
 				self.log("writing SNP merge records to the database ...")
 				self.addSNPMerges(listMerge)
-				listMerge = list()
 				self.log(" OK\n")
-				self.log("processing SNP merge records ...")
-		#foreach line in mergeFile
-		self.log(" OK: %d merged RS#s\n" % numMerge)
-		
-		# write any remaining records
-		if listMerge:
-			self.log("writing SNP merge records to the database ...")
-			self.addSNPMerges(listMerge)
-			self.log(" OK\n")
-		listMerge = None
+			listMerge = None
+		#if merges
 		
 		# process SNP role function codes
-		""" /* from dbSNP_main_table.sql.gz */
+		if options.get('roles','yes') == 'yes':
+			""" /* from dbSNP_main_table.sql.gz */
 CREATE TABLE [SnpFunctionCode]
 (
 [code] [tinyint] NOT NULL ,
@@ -151,23 +165,23 @@ CREATE TABLE [SnpFunctionCode]
 [SO_id] [varchar](32) NULL
 )
 """
-		self.log("processing SNP role codes ...")
-		roleID = dict()
-		codeFile = self.zfile('SnpFunctionCode.bcp.gz')
-		for line in codeFile:
-			words = line.split('\t')
-			code = int(words[0])
-			name = words[1]
-			desc = words[2]
-			coding = int(words[5]) if (len(words) > 5 and words[5] != '') else None
-			exon = int(words[6]) if (len(words) > 6 and words[6] != '') else None
+			self.log("processing SNP role codes ...")
+			roleID = dict()
+			codeFile = self.zfile('SnpFunctionCode.bcp.gz')
+			for line in codeFile:
+				words = line.split('\t')
+				code = int(words[0])
+				name = words[1]
+				desc = words[2]
+				coding = int(words[5]) if (len(words) > 5 and words[5] != '') else None
+				exon = int(words[6]) if (len(words) > 6 and words[6] != '') else None
+				
+				roleID[code] = self.addRole(name, desc, coding, exon)
+			#foreach line in codeFile
+			self.log(" OK: %d codes\n" % len(roleID))
 			
-			roleID[code] = self.addRole(name, desc, coding, exon)
-		#foreach line in codeFile
-		self.log(" OK: %d codes\n" % len(roleID))
-		
-		# process SNP roles
-		""" /* from human_9606_table.sql.gz */
+			# process SNP roles
+			""" /* from human_9606_table.sql.gz */
 CREATE TABLE [b137_SNPContigLocusId]
 (
 [snp_id] [int] NULL ,
@@ -200,52 +214,53 @@ CREATE TABLE [b137_SNPContigLocusId]
 [verComp] [int] NULL
 )
 """
-		self.log("processing SNP roles ...")
-		listRole = list()
-		numRole = 0
-		setOrphan = set()
-		numOrphan = 0
-		funcFile = self.zfile(self._identifyLatestSNPContig(os.listdir('.')))
-		for line in funcFile:
-			words = line.split("\t")
-			rs = long(words[0])
-			entrez = int(words[5])
-			#genesymbol = words[6]
-			code = int(words[11])
+			self.log("processing SNP roles ...")
+			listRole = list()
+			numRole = 0
+			setOrphan = set()
+			numOrphan = 0
+			funcFile = self.zfile(self._identifyLatestSNPContig(os.listdir('.')))
+			for line in funcFile:
+				words = line.split("\t")
+				rs = long(words[0])
+				entrez = int(words[5])
+				#genesymbol = words[6]
+				code = int(words[11])
+				
+				if code not in roleID:
+					setOrphan.add(code)
+					numOrphan += 1
+				else:
+					listRole.append( (rs,entrez,roleID[code]) )
+					numRole += 1
+				
+				# write to the database after each 2.5 million, to keep memory usage down
+				if len(listRole) >= 2500000:
+					self.log(" %1.1f million so far\n" % (numRole/1000000.0)) #TODO: time estimate
+					self.log("writing SNP roles to the database ...")
+					self.addSNPEntrezRoles(listRole)
+					listRole = list()
+					self.log(" OK\n")
+					self.log("processing SNP roles ...")
+			#foreach line in funcFile
+			self.log(" OK: %d roles\n" % (numRole,))
+			self.logPush()
+			if setOrphan:
+				self.log("WARNING: %d roles (%d codes) unrecognized\n" % (numOrphan,len(setOrphan)))
+			setOrphan = None
+			self.logPop()
 			
-			if code not in roleID:
-				setOrphan.add(code)
-				numOrphan += 1
-			else:
-				listRole.append( (rs,entrez,roleID[code]) )
-				numRole += 1
-			
-			# write to the database after each 2.5 million, to keep memory usage down
-			if len(listRole) >= 2500000:
-				self.log(" %1.1f million so far\n" % (numRole/1000000.0)) #TODO: time estimate
+			# write any remaining records
+			if listRole:
 				self.log("writing SNP roles to the database ...")
 				self.addSNPEntrezRoles(listRole)
-				listRole = list()
 				self.log(" OK\n")
-				self.log("processing SNP roles ...")
-		#foreach line in funcFile
-		self.log(" OK: %d roles\n" % (numRole,))
-		self.logPush()
-		if setOrphan:
-			self.log("WARNING: %d roles (%d codes) unrecognized\n" % (numOrphan,len(setOrphan)))
-		setOrphan = None
-		self.logPop()
-		
-		# write any remaining records
-		if listRole:
-			self.log("writing SNP roles to the database ...")
-			self.addSNPEntrezRoles(listRole)
-			self.log(" OK\n")
-		listRole = None
+			listRole = None
+		#if roles
 		
 		# process chromosome report files
 		grcBuild = None
-		snpLociValid = (options.get('snp-loci') == 'validated')
+		snpLociValid = (options.get('loci') == 'validated')
 		for fileChm in self._chmList:
 			self.log("processing chromosome %s SNPs ..." % fileChm)
 			chmFile = self.zfile('chr_%s.txt.gz' % fileChm)
