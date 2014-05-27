64732c542c2a2d74e3b09d9f9eaa2ea25d48191a
atfrase <atf3@psu.edu>
2014-05-27 15:56:02 +0000

    updated version tags to 2.2.0rc1
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/LOKI@3419 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- trunk/loki/loki_db.py
+++ trunk/loki/loki_db.py
@@ -17,7 +17,7 @@ class Database(object):
 	def getVersionTuple(cls):
 		# tuple = (major,minor,revision,dev,build,date)
 		# dev must be in ('a','b','rc','release') for lexicographic comparison
-		return (2,2,0,'b',1,'2014-05-20')
+		return (2,2,0,'rc',1,'2014-05-27')
 	#getVersionTuple()
 	
 	
